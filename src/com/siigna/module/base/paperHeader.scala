6c02947f835bed5da5743de35490883b93ce69d3
oleegholm <oleegholm@gmail.com>
2013-01-08 23:08:32 +0100

    updated performance in header

--- src/com/siigna/module/base/paperHeader.scala
+++ src/com/siigna/module/base/paperHeader.scala
@@ -20,37 +20,29 @@ import java.awt.Color
 
 object paperHeader {
   
-  println("drawing boundary: "+Drawing.boundary)
-  
-  def unitX(times : Int) = Vector2D(times * Siigna.paperScale, 0)
-  val boundary = Drawing.boundary // Get the boundary
-  val headerHeight = scala.math.min(boundary.height, boundary.width) * 0.025 // Define header
-  def transformation(t: TransformationMatrix) : TransformationMatrix = t.concatenate(TransformationMatrix(boundary.bottomRight - Vector2D(headerWidth * 0.99, -headerHeight * 0.8), 1))
-  val scale = TextShape("Scale 1:"+ (Siigna.paperScale), unitX(-10), headerHeight * 0.55) // Paper scale
-  val getURL = TextShape(" ", Vector2D(0, 0), headerHeight * 0.7)  // Get URL
-  val headerWidth  = (scale.boundary.width + getURL.boundary.width) * 1.2
+  // horizontal headerborder
+  def headerFrame (t : TransformationMatrix, s : Int, b: Rectangle2D) = {
+    val br = b.bottomRight
+    val bl = b.bottomLeft
+    val pt1 = br + Vector2D(0,(6*s))
+    val pt2 = Vector2D((br.x/2 + bl.x),br.y) + Vector2D(0,(6*s))
+    val pt3 = Vector2D((br.x/2 + bl.x),br.y)
+    PolylineShape(pt1,pt2,pt3).transform(t).setAttribute("StrokeWidth" -> 0.3)
+  }
 
-
-  //draw frame to indicate level of openness:
+  //a colored frame to indicate level of openness:
+  //TODO: make color dynamic on the basis of drawing level of openness
   def openness (t : TransformationMatrix, scale: Int, b: Rectangle2D) = {
     val oversize1 = (b.bottomLeft + Vector2D(-2 * scale, -2 * scale))
     val oversize2 = (b.topRight + Vector2D(2 * scale, 2 * scale))
     PolylineShape(Rectangle2D(oversize1, oversize2)).transform(t).setAttributes("Color" -> new Color(0.25f, 0.85f, 0.25f, 0.20f), "StrokeWidth" -> 4.0)
   }
-
-  // horizontal headerborder
-  //def headerFrame (t : TransformationMatrix, scale) = {
-  //  val br = boundary.bottomRight
-  //  val bl = boundary.bottomLeft
-  //  PolylineShape(
-  //    br + Vector2D(0,(6*(Siigna.paperScale))),
-  //    Vector2D((br.x/2 + bl.x),br.y) + Vector2D(0,(6*(Siigna.paperScale)))).transform(t).setAttribute("StrokeWidth" -> 0.3)
-  //}
-
-  // vertical headerborder
-  //def vertical (t : TransformationMatrix) = LineShape(, Vector2D((br.x/2 + bl.x),br.y) + Vector2D(0,(6*(Siigna.paperScale)))).transform(t).setAttribute("StrokeWidth" -> 0.3)
-
-  // paperScale - TODO: letter width: 50% letter spacing: 200%
+  // paper scale text - TODO: letter width: 50% letter spacing: 200%
+  def scaleText (t : TransformationMatrix, s : Int, b: Rectangle2D) = {
+    val br = b.bottomRight
+    TextShape("Scale 1:"+ s, br + Vector2D(-30*s,5.1*s), s * 4).transform(t) // Paper scale
+  }
+  //val getURL = TextShape(" ", Vector2D(0, 0), headerHeight * 0.7)  // Get URL
 
   //g draw separator
   //val seperator = (getURL.transform(transformation.translate(scale.boundary.topRight + unitX(4))))
